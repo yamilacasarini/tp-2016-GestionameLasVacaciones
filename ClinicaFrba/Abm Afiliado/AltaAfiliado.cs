7be4d8d3ad3ae516c222574145c8b315a217fdfe
Maria Luz <malupeb@hotmail.com.ar>
2016-10-20 00:17:19 -0300

    pushear es una verga

--- ClinicaFrba/Abm Afiliado/AltaAfiliado.cs
+++ ClinicaFrba/Abm Afiliado/AltaAfiliado.cs
@@ -7,14 +7,101 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace ClinicaFrba.Abm_Afiliado
 {
     public partial class AltaAfiliado : Form
     {
+
+        public static ClinicaFrba.Server server;
+        private SqlDataReader reader;
+
         public AltaAfiliado()
         {
             InitializeComponent();
+            llenarPlanes();
         }
-    }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        void llenarPlanes()
+        {
+            server = Server.getInstance();
+            reader = server.query("SELECT DISTINCT description FROM GESTIONAME_LAS_VACACIONES.Servicio");
+            
+            while(reader.Read()){
+                cBplanMedico.Items.Add(reader["description"].ToString());
+            }
+            reader.Close();
+        }
+
+        private void buttonModificar_Click(object sender, EventArgs e)
+        {
+            Abm_Afiliado.Principal proximo = new  Abm_Afiliado.Principal();
+            this.Close();
+            proximo.Show();
+            server.query("INSERT INTO GESTIONAME_LAS_VACACIONES.Paciente(nombre,apellido,documento,direccion,telefono,email,fechaNacimiento,sexo,EstadoCivil,cantFamiliares)" +
+            "VALUES ("+ txNombre.Text.Trim() +", "+ txApellido.Text.Trim() +","+ txDocumento.Text.Trim() +","+ txDireccion.Text.Trim() +","+ txTelefono.Text.Trim() +
+            " "+ txMail.Text.Trim() +","+ dateTimePicker1.Text.Trim() +","+ cBsexo.Text.Trim() +","+ cBestadoCivil.Text.Trim() +","+ txFamiliaresACargo.Text.Trim() +",)"
+        private void buttonAceptar_Click(object sender, EventArgs e)
+        {
+            if(!validarDatos()){
+            }
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void txNombre_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void txDireccion_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+        bool validarDatos(){
+           return txNombre.Text.Trim() != "" && txApellido.Text.Trim() != "" &&
+               txTelefono.Text.Trim() != "" && txMail.Text.Trim() != "" && 
+               txDireccion.Text.Trim() != "" && txDocumento.Text.Trim() != "";
+        }
+
+        private void cBtipoDocumento_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        
+        }
 }
