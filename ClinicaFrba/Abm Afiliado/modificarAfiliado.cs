d0a807ea3c18f94f657b23a69123af822325eef8
mapeb <malupeb@hotmail.com.ar>
2016-10-26 20:16:24 -0300

    vuelve a la vida

---
+++ ClinicaFrba/Abm Afiliado/modificarAfiliado.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ClinicaFrba.Abm_Afiliado
+{
+
+    public partial class modificarAfiliado : Form
+    {
+        dataClass.afiliado afiliado = new dataClass.afiliado();
+        public modificarAfiliado()
+        {
+            InitializeComponent();
+            btAceptar.Hide();
+            btCambiarPlan.Hide();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btBuscar_Click(object sender, EventArgs e)
+        {
+            Server server = Server.getInstance();
+            SqlDataReader reader;
+            listadoAfiliados formAfiliados = new listadoAfiliados();
+            formAfiliados.ShowDialog();
+            if (formAfiliados.afiliadoBuscado.id != -1)
+            {
+                afiliado = formAfiliados.afiliadoBuscado;
+                txApellido.Text = afiliado.apellido;
+                txNombre.Text = afiliado.nombre;
+                txDireccion.Text = afiliado.direccion;
+                txDocumento.Text = Convert.ToString(afiliado.documento);
+                txFamiliaresACargo.Text = Convert.ToString(afiliado.cantFamiliares);
+                txMail.Text = afiliado.email;
+                txTelefono.Text = Convert.ToString(afiliado.telefono);
+                cBestadoCivil.Text = afiliado.estadoCivil;
+                cBsexo.Text = afiliado.sexo;
+                cBtipoDocumento.Text = afiliado.tipoDocumento;
+                reader = server.query("SELECT GESTIONAME_LAS_VACACIONES.getPlanMedico(" + formAfiliados.afiliadoBuscado.id + ")");
+                txPlanMedico.Text = reader.GetString(0); // espero que funcione
+            }
+            btAceptar.Show();
+            btCambiarPlan.Show();
+        }
+
+        private void cBestadoCivil_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAceptar_Click(object sender, EventArgs e)
+        {
+            if (validarDatos())
+            {
+                Server server = Server.getInstance();
+                server.query("UPDATE GESTIONAME_LAS_VACACIONES.Paciente SET direccion =" +
+             txDireccion.Text.Trim() + ", telefono =" + txTelefono.Text.Trim() + ", email =" + txMail.Text.Trim() + ", sexo=" +
+             cBsexo.Text.Trim() + ", estadoCivil = " + cBestadoCivil.Text.Trim() + ", cantFamiliares = "+ txFamiliaresACargo.Text.Trim() +
+                 "WHERE id =" + afiliado.id);
+                this.Close();
+        
+            }
+            else {
+            MessageBox.Show("Falta algun dato");
+            }
+        }
+
+        private void txDocumento_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        bool validarDatos()
+        {
+            return txNombre.Text.Trim() != "" && txApellido.Text.Trim() != "" &&
+                txTelefono.Text.Trim() != "" && txMail.Text.Trim() != "" &&
+                txDireccion.Text.Trim() != "" && txDocumento.Text.Trim() != "" &&
+                cBestadoCivil.Text.Trim() != "" &&
+                cBsexo.Text.Trim() != "" && cBtipoDocumento.Text.Trim() != "";
+        }
+
+        private void txNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txPlanMedico_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btCambiarPlan_Click(object sender, EventArgs e)
+        {
+            cambiarPlanMedico form = new cambiarPlanMedico();
+            form.afiliado = afiliado;
+            this.Hide();
+            form.ShowDialog();
+            btCambiarPlan.Hide();
+            this.Show(); // hipoteticamente dicen que esto sucede recien cuando el form se cierra
+        }
+    }
+}
