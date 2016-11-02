476847aae807346d9c7ca18ab5e48b2e6f8ab89e
FrankIglesias <franciscoj.iglesias@hotmail.com>
2016-11-02 04:15:07 -0300

    arreglos de abm afiliado

--- ClinicaFrba/Registro Llegada/buscarTurno.Designer.cs
+++ ClinicaFrba/Registro Llegada/buscarTurno.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ClinicaFrba.Registro_Llegada
 {
-    partial class buscarProfesional
+    partial class buscarTurno
     {
         /// <summary>
         /// Required designer variable.
@@ -39,6 +39,12 @@
             this.btCancelar = new System.Windows.Forms.Button();
             this.btSeleccionar = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,16 +63,16 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label4.Location = new System.Drawing.Point(150, 9);
+            this.label4.Location = new System.Drawing.Point(224, 9);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(240, 20);
+            this.label4.Size = new System.Drawing.Size(100, 20);
             this.label4.TabIndex = 35;
-            this.label4.Text = "Buscar un medico para antender";
+            this.label4.Text = "Buscar turno";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // txEspecialidad
             // 
-            this.txEspecialidad.Location = new System.Drawing.Point(154, 97);
+            this.txEspecialidad.Location = new System.Drawing.Point(142, 93);
             this.txEspecialidad.Name = "txEspecialidad";
             this.txEspecialidad.Size = new System.Drawing.Size(100, 20);
             this.txEspecialidad.TabIndex = 34;
@@ -76,7 +82,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label3.Location = new System.Drawing.Point(53, 95);
+            this.label3.Location = new System.Drawing.Point(16, 93);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(99, 20);
             this.label3.TabIndex = 33;
@@ -84,7 +90,7 @@
             // 
             // txApellido
             // 
-            this.txApellido.Location = new System.Drawing.Point(154, 71);
+            this.txApellido.Location = new System.Drawing.Point(142, 67);
             this.txApellido.Name = "txApellido";
             this.txApellido.Size = new System.Drawing.Size(100, 20);
             this.txApellido.TabIndex = 32;
@@ -93,15 +99,15 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label1.Location = new System.Drawing.Point(53, 69);
+            this.label1.Location = new System.Drawing.Point(16, 67);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.Size = new System.Drawing.Size(120, 20);
             this.label1.TabIndex = 31;
-            this.label1.Text = "Apellido";
+            this.label1.Text = "Apellido Medico";
             // 
             // txNombre
             // 
-            this.txNombre.Location = new System.Drawing.Point(154, 45);
+            this.txNombre.Location = new System.Drawing.Point(142, 41);
             this.txNombre.Name = "txNombre";
             this.txNombre.Size = new System.Drawing.Size(100, 20);
             this.txNombre.TabIndex = 30;
@@ -110,11 +116,11 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label2.Location = new System.Drawing.Point(53, 43);
+            this.label2.Location = new System.Drawing.Point(16, 41);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.Size = new System.Drawing.Size(120, 20);
             this.label2.TabIndex = 29;
-            this.label2.Text = "Nombre";
+            this.label2.Text = "Nombre Medico";
             // 
             // btCancelar
             // 
@@ -149,11 +155,68 @@
             this.dataGridView1.Size = new System.Drawing.Size(463, 148);
             this.dataGridView1.TabIndex = 26;
             // 
-            // buscarProfesional
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(415, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 42;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(269, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 20);
+            this.label5.TabIndex = 41;
+            this.label5.Text = "Id turno";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(415, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 40;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(269, 65);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(131, 20);
+            this.label6.TabIndex = 39;
+            this.label6.Text = "Apellido Paciente";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(415, 43);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 38;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label7.Location = new System.Drawing.Point(269, 39);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(131, 20);
+            this.label7.TabIndex = 37;
+            this.label7.Text = "Nombre Paciente";
+            // 
+            // buscarTurno
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(537, 379);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.txEspecialidad);
@@ -165,7 +228,7 @@
             this.Controls.Add(this.btCancelar);
             this.Controls.Add(this.btSeleccionar);
             this.Controls.Add(this.dataGridView1);
-            this.Name = "buscarProfesional";
+            this.Name = "buscarTurno";
             this.Text = "buscarProfesional";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
@@ -186,5 +249,11 @@
         private System.Windows.Forms.Button btCancelar;
         private System.Windows.Forms.Button btSeleccionar;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label7;
     }
 }
