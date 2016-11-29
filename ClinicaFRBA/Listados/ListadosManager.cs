db41c8b0e3b96752db8f4b06f27424c31966493e
marilu <malupeb@hotmail.com.ar>
2016-11-29 01:10:10 -0300

    arreglado menos horas

--- ClinicaFRBA/Listados/ListadosManager.cs
+++ ClinicaFRBA/Listados/ListadosManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -59,13 +59,44 @@ namespace ClinicaFrba.Listados
 
             List<Especialidad> especialidades = new List<Especialidad>();
             Server server = Server.getInstance();
-            SqlDataReader reader = server.query("select  id,descripcion from GESTIONAME_LAS_VACACIONES.Planes");
+            SqlDataReader reader = server.query("select  id,descripcion from GESTIONAME_LAS_VACACIONES.Especialidades");
             while (reader.Read())
             {
                 Especialidad esp = new Especialidad();
+                esp.id = Convert.ToInt32(reader["id"]);
+                esp.descripcion = reader["descripcion"].ToString();
+                especialidades.Add(esp);
             }
             reader.Close();
             return especialidades;
+        }
+        public static List<profMenosHoras> obtenerProfesionalesConMenosHoras(string descPlan, string descEspecialidad, DateTime desde, DateTime hasta)
+        {
+            List<profMenosHoras> profs = new List<profMenosHoras>();
+            Server server = Server.getInstance();
+            string query = "select id from GESTIONAME_LAS_VACACIONES.Planes where descripcion like '" + descPlan + "'";
+            SqlDataReader reader = server.query(query);
+            reader.Read();
+            int plan = Convert.ToInt32(reader["id"]);
+            reader.Close();
+            query = "select id from GESTIONAME_LAS_VACACIONES.Especialidades where descripcion like '" + descEspecialidad + "'";
+            SqlDataReader reader1 = server.query(query);
+            reader1.Read();
+            int especialidad = Convert.ToInt32(reader1["id"]);
+            reader1.Close();
+            query = "select  * from GESTIONAME_LAS_VACACIONES.topProfesionalesConMenosHoras(" + plan + "," +
+                  especialidad + ",'" + desde.ToString() + "','" + hasta.ToString() + "')";
+            SqlDataReader reader3 = server.query(query);
+            while (reader3.Read())
+            {
+                profMenosHoras prof = new profMenosHoras();
+                prof.horas = Convert.ToInt32(reader3[0]);
+                prof.nombre = Convert.ToString(reader3[1]);
+                prof.apellido = Convert.ToString(reader3[2]);
+                profs.Add(prof);
+            }
+            reader3.Close();
+            return profs;
         }
            
            
