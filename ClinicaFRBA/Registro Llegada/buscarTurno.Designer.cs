b078a6eb99a2c9803ad00cc13fc5589898db2adb
FrankIglesias <frank.beatle@gmail.com>
2016-12-09 05:08:00 -0300

    ultimos cambios de la noche

--- ClinicaFRBA/Registro Llegada/buscarTurno.Designer.cs
+++ ClinicaFRBA/Registro Llegada/buscarTurno.Designer.cs
@@ -166,7 +166,6 @@
             // 
             // cbEspecialidad
             // 
-            this.cbEspecialidad.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbEspecialidad.FormattingEnabled = true;
             this.cbEspecialidad.Location = new System.Drawing.Point(411, 69);
             this.cbEspecialidad.Name = "cbEspecialidad";
